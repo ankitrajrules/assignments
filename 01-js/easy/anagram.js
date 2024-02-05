15d04051032e01f8e3d8b583e396a9f8174ea507
ankitrajrules <121212804+ankitrajrules@users.noreply.github.com>
2024-02-05 22:22:39 +0530

    Update anagram.js
    i used blackbox aii for comments for learning purposes

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+    // Check if lengths of both strings are same
+    if (str1.length !== str2.length) return false; // If lengths aren't equal
+    else {
+        if (str1.split('').sort().join('') === str2.split('').sort().join('')){
+            return true; // Return True if sorted string matches
+        }
+        else
+            return false;
+    }
 }
 
 module.exports = isAnagram;
